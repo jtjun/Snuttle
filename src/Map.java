57140d7c8d63916572122dfec835d179f7971953
Young-in <daddy321@snu.ac.kr>
2018-05-03 17:29:22 +0900

    Generator K hotspots 추가

--- src/Map.java
+++ src/Map.java
@@ -1,4 +1,13 @@
 public class Map{
     private Station[] stations;
+
+    public Map(){
+        stations = new Station[Simulator.MAX_STATION];
+    }
     
+    public Station getStation(int idx){
+        if(idx < 0) throw new IndexOutOfBoundsException();
+        if(idx >= Simulator.MAX_STATION) throw new IndexOutOfBoundsException();
+        return stations[idx];
+    }
 }
--- src/Simulator.java
+++ src/Simulator.java
@@ -1,3 +1,12 @@
 public class Simulator {
     public static int MAX_TIME = 100;
+    public static int MAX_STATION = 10;
+    public static double K_RATIO = 0.5;
+
+    public Simulator(){
+        int userN = 1000;
+
+        Map map = new Map();
+        Generator generator = new Generator(userN, map);
+    }
 }
