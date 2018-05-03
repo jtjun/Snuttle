93442e430b5552d793ec3834ce292a17c71386fb
jtjun <tae7130@naver.com>
2018-05-03 17:45:34 +0900

    ㅇㅇ

--- src/Map.java
+++ src/Map.java
@@ -1,10 +1,19 @@
 public class Map{
     private Station[] stations;
 
+
     public Station getStation(String name){
         for(int i=0; i<stations.length; i++ ){
             if(name.equals(stations[i].getName())) return stations[i];
         } return error; // error
+
+    public Map(){
+        stations = new Station[Simulator.MAX_STATION];
     }
     
+    public Station getStation(int idx){
+        if(idx < 0) throw new IndexOutOfBoundsException();
+        if(idx >= Simulator.MAX_STATION) throw new IndexOutOfBoundsException();
+        return stations[idx];
+    }
 }
