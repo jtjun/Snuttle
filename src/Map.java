f570454858373486fbcbc13bd2a714c9ec400871
Young-in <daddy321@snu.ac.kr>
2018-05-03 23:37:06 +0900

    조금 수정 및 Simulator 확인

--- src/Map.java
+++ src/Map.java
@@ -35,8 +35,12 @@ public class Map {
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
-                        dist[i][j] = dist[i][k] + dist[k][j];
+                    if (dist[i][k]!=-1 && dist[k][j]!=-1){
+                        if(dist[i][j]==-1){
+                            dist[i][j] = dist[i][k] + dist[k][j];
+                        }else if(dist[i][j] > dist[i][k] + dist[k][j]) {
+                            dist[i][j] = dist[i][k] + dist[k][j];
+                        }
                     }
                 }
             }
