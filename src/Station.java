1254fe3e45232b3d79f933440f3f6ad115bcfb3f
jtjun <tae7130@naver.com>
2018-05-03 14:43:21 +0900

    Station

--- src/Station.java
+++ src/Station.java
@@ -1,2 +1,22 @@
 public class Station {
+
+    private String name;
+    private double x;
+    private double y;
+
+    Station(String namei, double xi, double, yi){
+        name = namei;
+        x = xi;
+        y = yi;
+    }
+
+    public int getName(){
+        return name;
+    }
+
+    public double[] getPosition(){
+        return {x, y};
+    }
+
+    //public Station nearestStation(double x, double y){}
 }
