876268347b2b03e36e0428d5b922366adab3cd47
Young-in <daddy321@snu.ac.kr>
2018-05-11 13:57:29 +0900

    trampsteamer 초기값 추가

--- src/TrampSteamerGreedy.java
+++ src/TrampSteamerGreedy.java
@@ -2,6 +2,40 @@ import java.util.*;
 
 public class TrampSteamerGreedy{
     public static ArrayList<Integer> cycle;
+    
+    public static void setGreedySchedule(Shuttle[] shuttles, Map map, ArrayList<Guest> guests, int fixedshuttle){
+        
+        int n = map.getNumStations();
+        int[] stationorder = {0, 2, 3, 4, 20, 6, 7, 8, 9, 10, 22, 19, 18, 17, 21, 16, 15, 13, 12, 11, 14, 5, 1};
+
+        for(int i = 0; i < shuttles.length; i++){ // For each shuttle,
+            int x = 15;
+            int y = 20;
+            int t = 0; // acculated time for shuttle
+
+            // Make new schedule
+            Schedule schedule = new Schedule();
+            int startStation = n/shuttles.length * i; // Start at different positions
+            schedule.addSchedule(t,map.getStation(stationorder[startStation]), 0);
+            if(Simulator.Wait) {
+                t+=1;
+                schedule.addSchedule(t,map.getStation(stationorder[startStation]), 0);
+            }
+
+            for(int j = startStation+1;/* j < startStation+n*10*/ t<=Simulator.MAX_TIME; j++){ // Visit each node for 10 cycles
+                t += map.getDistance(stationorder[(j-1)%n], stationorder[j%n]);
+                if(t>Simulator.MAX_TIME) break;
+                schedule.addSchedule(t, map.getStation(stationorder[j%n]), 0);
+                if(Simulator.Wait) {
+                    t+=1;
+                    schedule.addSchedule(t, map.getStation(stationorder[j%n]), 0);
+                }
+            }
+            shuttles[i] = new Shuttle(map.getStation(stationorder[startStation]).getX(),
+                                      map.getStation(stationorder[startStation]).getY(),
+                                     0, schedule, i, map);
+        }
+    }
     public static void setIGreedyEach(Shuttle[] shuttles, int shuttlenum, int time){
         int n = Simulator.guests.size();
         int[][] requests = new int[n][n];
